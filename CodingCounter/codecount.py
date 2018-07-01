b5c21e9c3589c8d4087f59441ff14d8bf17d96d1
Eyc <1911362549@qq.com>
2018-07-01 08:27:33 -0700

    小节

--- CodingCounter/codecount.py
+++ CodingCounter/codecount.py
@@ -5,7 +5,7 @@ import os.path as osp
 from config import config
 
 Ques = collections.namedtuple(
-    'Ques',['type','year','month','day','time','name'])
+    'Ques', ['type', 'year', 'month', 'day', 'time', 'name'])
 
 
 class Ques_Set:
@@ -14,23 +14,26 @@ class Ques_Set:
         self._Ques_set = []
 
     def add_Ques(self, qtype, year, month, day, time, name):
-        self._Ques_set.append(Ques(qtype,year,month,day,time,name))
+        self._Ques_set.append(Ques(qtype, year, month, day, time, name))
 
     def get_Ques(self):
         for code in config['QUES']['available'].split(','):
-            path = osp.join(osp.abspath(osp.dirname(__file__)),code)
+            path = osp.join(osp.abspath(osp.dirname(
+                osp.dirname(__file__))), code)
             ques_info = subprocess.check_output(
-                ['ls','-l','--full-time',path]).decode('utf-8').split()[2:]
-            for i in range(0,len(ques_info),9):
+                ['ls', '-l', '--full-time', path]).decode('utf-8').split()[2:]
+            for i in range(0, len(ques_info), 9):
                 year, month, day = ques_info[i+5].split('-')
-                Ques_Set.add_Ques(self, code, year, month, day, ques_info[i+6],
-                    ques_info[i+8])
+                Ques_Set.add_Ques(
+                    self,
+                    code, year, month, day, ques_info[i+6], ques_info[i+8])
 
     def __len__(self):
         return len(self._Ques_set)
 
     def __getitem__(self, position):
         return self._Ques_set[position]
+
 
 def main():
     q = Ques_Set()
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-# CodingCount for programmer
+### CodingCount for programmer
+
+> 统计代码使用教程
+>* 将 CodingCounter 文件夹放入代码仓库中
+>* 在CodingCount.ini 配置文件中写入需要管理自己的仓库名称
+>* 使用Ques_Set 类即可管理信息
+
+### 更多内容待开发
+
+> 代码仓库实时更新脚本（已完成，未部署）
+> 可视化代码管理
+> 网站一键部署
